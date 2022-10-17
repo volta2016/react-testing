bf5f3a293db845e73d2c86f26e15b70d7e825d00
volta2016 <giovoltadesign@gmail.com>
2022-10-17 19:12:47 -0300

    [testing] structure-of-a-unit-test

--- first-unit-test/src/App.test.js
+++ first-unit-test/src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { create } from "react-test-renderer";
+import React from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("<App />", () => {
+  it("renderizar correctamente", () => {
+    const component = create(<App />);
+    expect(component).toBeDefined();
+  });
 });
